7ee86b5a90833c20cd69c13f928425ba9bea43ab
Santiago Quintero <santiago.quintero@inria.fr>
2019-09-26 16:43:50 +0200

    Added distributive lattice generation
    Also added a experimental Lattice to calculate space functions in parallel

---
+++ experimental.py
@@ -0,0 +1,193 @@
+import multiprocessing as mp
+from itertools import combinations, product
+from graphviz import Digraph
+from delta import calculate_lubs, calculate_glbs
+from lattice import covers_from_lattice, lattice_from_covers
+from generation import is_distributive
+from time import perf_counter
+
+# ######################################
+# Lattice object, functions, and methods
+# ######################################
+
+class NxLattice:
+    def __init__(self, implies_matrix, parallel=False):
+        """ Create a lattice from a matrix of implications,
+            where implies_matrix[a][b] == 1, means that a >= b.
+
+            Also calculates the corresponding matrix of
+            least upper bounds and greatest lower bounds.
+        """
+        self.lattice = implies_matrix
+        self.lubs = calculate_lubs(implies_matrix)
+        self.glbs = calculate_glbs(implies_matrix)
+        self.parallel = parallel # Added for parallel execution
+        # Lazy attributes
+        self._space_functions = None
+        self._impls = None
+
+    def __len__(self):
+        """ Returns the number of nodes in the lattice.
+        """
+        return len(self.lattice)
+
+    def is_lattice(self):
+        """ Returns True if the lattice is valid.
+        """
+        N = len(self)
+        # List of pairs, leaving them as an iterator allows us
+        # to short-circuit and not generate all the pairs if
+        # the lattice is not valid.
+        lt = combinations(range(N),2)
+        return all(self.lubs[a][b] != -1 for (a, b) in lt)
+    
+    def is_fn_distributive(self, fn, test_pairs):
+        """ Checks if a given function `fn` is distributive.
+        """
+        return all(fn[self.lubs[t0][t1]] == self.lubs[fn[t0]][fn[t1]] for t0, t1 in test_pairs)
+
+    def is_fn_distributive_packed(self, pack):
+        """ Equivalent to is_fn_distributive but all arguments are `pack`ed into one.
+            It also returns the element checked and the result.
+            Usefull for parallel computation using map, imap and equivalents.
+        """
+        fn, test_pairs = pack
+        return (fn, self.is_fn_distributive(fn, test_pairs))
+
+    def is_fn_monotonic(self, fn):
+        """ Checks if a given function `fn` is monotonic.
+        """
+        n = len(self)
+        return all(self.lattice[fn[a]][fn[b]] == 1 for a in range(n) for b in range(n) if self.lattice[a][b] == 1)
+
+    def monotonic_functions(self):
+        """ Generate all bottom-preserving monotonic functions of `lattice`.
+        """
+        n = len(self)
+        return [(0,) + fn for fn in product(range(n), repeat=n-1) if self.is_fn_monotonic((0,) + fn)]
+
+    def lub(self, iterable):
+        """ Least Upper Bound of `iterable`.
+        """
+        r = 0
+        for i in iterable:
+            r = self.lubs[r][i]
+        return r
+
+    def glb(self, iterable):
+        """ Greatest Lower Bound of `iterable`.
+        """
+        r = len(self) - 1
+        for i in iterable:
+            r = self.glbs[r][i]
+        return r
+
+    def imply(self, a, b):
+        """ Returns a imply b.
+        """
+        return self.impls[a][b]
+
+    def atoms(self):
+        """ Returns a list of all the atoms.
+            
+            + `y` is an atom if `0` is covered by `y`
+            + `x` is covered by `y` if `x < y` and `x <= z < y` implies `z = x`
+        """
+        n = len(self)
+        return [i for i in range(n) if all(i != 0 and (i == j or j == 0 or self.lattice[i][j] == 0) for j in range(n))]
+
+    def join_irreducible_elements(self):
+        """ Returns a list of all the join-irreducible elements in the lattice.
+
+            `x` is join-irreducible (or lub-irreducible) if:
+            + `x != 0`
+            + `a < x` and `b < x` imply `a lub b < x` for all `a` and `b` 
+        """
+        n = len(self)
+        test_pairs = tuple(combinations(range(n), 2))
+        return [x for x in range(1, n) if all((x == a or x == b or x != self.lubs[a][b] for a, b in test_pairs))]
+
+    @property
+    def space_functions(self):
+        """ Return the list of space functions, based on the lattice.
+
+            The actual `space_functions` are only generated once, feel free
+            to call this method multiple times.
+        """
+        if self._space_functions is None:
+           self._space_functions = self._generate_space_functions()
+        return self._space_functions
+
+    @property
+    def impls(self):
+        """ Return the matrix of implications, based on the lattice.
+
+            The actual `implys` are only generated once, feel free
+            to call this method multiple times.
+        """
+        if self._impls is None:
+           self._impls = self._calculate_implications()
+        return self._impls
+
+    def diagram(self, space_function=None):
+        """ Returns the graphviz Digraph representation of the lattice for
+            further manipulation or DOT language representation.
+        """
+        graph = Digraph("Lattice",edge_attr={"arrowhead": "none"})
+        for i in range(len(self)):
+            graph.node(str(i))
+        for pos, nodes in enumerate(covers_from_lattice(self.lattice)):
+            for node in nodes:
+                graph.edge(str(pos), str(node))
+        if space_function is not None:
+            graph.attr("edge", arrowhead="normal", color="blue", constraint="false")
+            for pos, val in enumerate(space_function):
+                graph.edge(str(pos), str(val))
+        return graph
+
+    def _generate_space_functions(self):
+        """ Generate a list of space functions, based on the lattice.
+        """
+        N = len(self)
+        test_pairs = tuple(combinations(range(N), 2))
+        if self.parallel:
+            pool = mp.Pool(mp.cpu_count())
+            result = [fn for fn, keep in pool.imap_unordered(self.is_fn_distributive_packed, (((0,) + f, test_pairs) for f in product(range(N), repeat=N-1)), 1000) if keep]
+            pool.close()
+            return result
+        else:
+            return [(0,) + fn for fn in product(range(N), repeat=N-1) if self.is_fn_distributive((0,) + fn, test_pairs)]
+
+    def _calculate_implications(self):
+        """ Calculate the matrix of implications for the lattice.
+        """
+        N = len(self)
+        return [[self._pair_implication(i, j) for j in range(N)] for i in range(N)]
+    
+    def _pair_implication(self, a, b):
+        """ Calculate the greatest lower bound of the pair (`a`, `b`)
+            in the lattice
+        """
+        # a -> b ::= glb_{i <= b} { i | a lub i >= b }
+        return self.glb((i for i in range(len(self)) if self.lattice[b][i] == 1 and self.lattice[self.lubs[a][i]][b] == 1))
+
+# ######################################
+
+if __name__ == "__main__":
+    matrix = lattice_from_covers([[], [0], [0], [1,2]])
+    normal_lattice = NxLattice(matrix)
+    print("Is ditributive", is_distributive(normal_lattice))
+    parallel_lattice = NxLattice(matrix, True)
+    start = perf_counter()
+    sf = len(normal_lattice.space_functions)
+    finish = perf_counter() - start
+    print("Normal lattice space functions ({}) time: {}".format(sf, finish))
+    start = perf_counter()
+    sf = len(parallel_lattice.space_functions)
+    finish = perf_counter() - start
+    print("Parallel lattice space functions ({}) time: {}".format(sf, finish))
+    start = perf_counter()
+    monotonic = parallel_lattice.monotonic_functions()
+    finish = perf_counter() - start
+    print("Parallel lattice monotonic functions ({}) time: {}".format(len(monotonic), finish))
+    print(monotonic)
---
+++ generation.py
@@ -0,0 +1,179 @@
+import sys
+import random
+from itertools import combinations, product
+from lattice import Lattice, covers_from_lattice
+
+def progress_bar(value, end, length=20):
+    percent = value * 100 // end
+    bar = '=' * (percent * length // 100)
+    spaces = ' ' * (length - len(bar))
+    print("\rProgress: [{}] {}/{}".format(bar + spaces, value, end), end='', file=sys.stderr, flush=True)
+
+def print_distribution(dist, length=20, extra=None):
+    top = max(dist.values())
+    for d in sorted(dist):
+        value = dist[d]
+        bar = '-' * (value * length // top)
+        spaces = ' ' * (length - len(bar))
+        extra_info = ''
+        if extra is not None:
+            for entry in extra[d]:
+                extra_info += "({} {})".format(entry, extra[d][entry])
+        print("{} | {}({}){}".format(spaces + bar, d, value, extra_info))
+
+def binomial(n, k):
+    """ Calculates the binomial coefficient using the multiplicative formula.
+    """
+    if k < 0 or k > n:
+        return 0
+    result = 1
+    for i in range(1, min(k, n - k) + 1):
+        result = result * (n + 1 - i) // i
+    return result
+
+def is_distributive(lattice):
+    """ Test if the `lattice` is a distributive lattice.
+    """
+    n = len(lattice)
+    pairs = combinations(range(n), 2)
+    return all(lattice.glbs[lattice.lubs[a][b]][x] == lattice.lubs[lattice.glbs[a][x]][lattice.glbs[b][x]]
+        for a, b in pairs for x in range(n))
+
+
+def generate_subsets(n, p):
+    """ Generates up to 2^`n` subsets with probability `p` of keeping a subset.
+    """
+    result = set()
+    population = tuple(range(n))
+    for _ in range(2**n):
+        if random.random() < p:
+            element = random.sample(population, random.randrange(0, n))
+            result.add(frozenset(element))
+    return result
+
+def add_missing_subsets(subsets):
+    """ Adds missing subsets so that it is closed under Union and Intersection.
+    """
+    queue = combinations(subsets, 2)
+    while True:
+        new_elements = []
+        for a, b in queue:
+            union = a | b
+            intersection = a & b
+            if union not in subsets:
+                subsets.add(union)
+                new_elements.append(union)
+            if intersection not in subsets:
+                subsets.add(intersection)
+                new_elements.append(intersection)
+        if len(new_elements) == 0:
+            break
+        queue = product(new_elements, subsets)
+    return subsets
+
+
+def distributive_lattice(n, p):
+    """ Generates a random distributive lattice.
+
+        The generated lattice can be used with the `Lattice` class, or
+        converted with the `covers_from_lattice` function.
+
+        INPUTS:
+        + `n`: Number of elements of the population used to create the lattice.
+        + `p`: Probability, from 0 to 1, for keeping a node.
+    """
+    assert(n>0)
+    # Generate a random set of sets and close it under union and intersection.
+    subsets = list(add_missing_subsets(generate_subsets(n, p)))
+
+    if len(subsets) == 0:
+        subsets = [frozenset()]
+
+    # Find and reposition the top and bottom element
+    # This is required because the bottom is asumed to be the first element,
+    # and the top to be the last, this is an implementation detail that
+    # may be removed in a revised version of the lattice class.
+    n = len(subsets) - 1
+
+    top_pos = subsets.index(max(subsets, key=len))
+    subsets[top_pos], subsets[n] = subsets[n], subsets[top_pos]
+    bottom_pos = subsets.index(min(subsets, key=len))
+    subsets[bottom_pos], subsets[0] = subsets[0], subsets[bottom_pos]
+
+    # Conver the set into a lattice's matrix representation
+    result = [[0 for _ in range(len(subsets))] for _ in range(len(subsets))]
+    for i, row in enumerate(result):
+        for j, _ in enumerate(row):
+            if subsets[j].issubset(subsets[i]):
+                result[i][j] = 1
+    return result
+
+def test_distribution(n, p, samples):
+    distribution = {}
+    print("Running with n={} p={} and {} samples".format(n, p, samples))
+    progress_bar(0, samples, 50)
+    for i in range(1, samples + 1):
+        if i % 1000 == 0:
+            progress_bar(i, samples, 50)
+        size = len(distributive_lattice(n, p))
+        distribution[size] = distribution.get(size, 0) + 1
+    print(" Done.", file=sys.stderr, flush=True)
+    print_distribution(distribution, 50)
+
+
+def test_distributive_lattices(n, p, limit, samples):
+    from experimental import NxLattice
+    distribution = {}
+    extra = {}
+    skipped = 0
+    invalid = 0
+    exceptions = 0
+    print("Running with n={} p={} and a size limit of {}".format(n, p, limit))
+    for i in range(1, samples + 1):
+        progress_bar(i, samples, 40)
+        random_lattice = distributive_lattice(n, p)
+        size = len(random_lattice)
+        if size > limit:
+            skipped += 1
+        else:
+            distribution[size] = distribution.get(size, 0) + 1
+            if size > 7:
+                random_lattice = NxLattice(random_lattice, parallel=True)
+            else:
+                random_lattice = NxLattice(random_lattice, parallel=False)
+            if is_distributive(random_lattice):
+                ln_sf = binomial(2 * size - 2, size - 1)
+                sf = len(random_lattice.space_functions)
+                if size not in extra:
+                    extra[size] = {"min": sf, "max": sf, "Ln": ln_sf}
+                else:
+                    if extra[size]["min"] > sf:
+                        extra[size]["min"] = sf
+                    if extra[size]["max"] < sf:
+                        extra[size]["max"] = sf
+                if ln_sf < sf:
+                    exceptions += 1
+                    print("\n* Lattice {} : {}".format(len(random_lattice), covers_from_lattice(random_lattice)))
+                    print("+ Found {} (vs {} for Ln)".format(sf, ln_sf))
+            else:
+                invalid += 1
+    print(" Done.", file=sys.stderr, flush=True)
+    print("\nFinished with {} tests, {} skipped, {} invalid lattices".format(samples - skipped - invalid, skipped, invalid))
+    print("Lattices with more SF than Ln:", exceptions)
+    print("Distribution of Lattices by size:")
+    print_distribution(distribution, 40, extra)
+
+if __name__ == "__main__":
+    from delta import lattice_square, lattice_m
+    from lattice import test_equality
+    square = Lattice(lattice_square())
+    m3 = Lattice(lattice_m(5))
+    print("Running pre-checks")
+    # Testing binomial implementation
+    test_equality(20, binomial(6, 3), "binomial 6 3 is 20")
+    # Testing is_distributive
+    test_equality(True, is_distributive(square), "is_distributive square is True")
+    test_equality(False, is_distributive(m3), "is_distributive m_3 is False")
+
+    test_distributive_lattices(5, 0.2, 9, 100)
+    # test_distribution(4, 0.3, 100000)
